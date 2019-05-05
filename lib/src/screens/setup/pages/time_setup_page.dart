9130c2a70e9ddbb9df76c91c8b203ca0642dc890
Andrey Chernoprudov <achernoprudov@naumen.ru>
2019-05-05 10:12:28 +0500

    picker

--- lib/src/screens/setup/pages/time_setup_page.dart
+++ lib/src/screens/setup/pages/time_setup_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:russian_hat/src/screens/setup/models/duration_ui_model.dart';
 
-class SetupTimePage extends StatelessWidget {
+class TimeSetupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
@@ -10,15 +10,25 @@ class SetupTimePage extends StatelessWidget {
       children: <Widget>[
         Text(
           'Select round duration',
-          style: textTheme.display2,
+          style: textTheme.display3,
         ),
         Center(
           child: CupertinoPicker.builder(
-            itemExtent: 8,
+            itemExtent: 48,
+            diameterRatio: 1.3,
+            useMagnifier: false,
+            backgroundColor: Colors.transparent,
             childCount: DurationUiModel.all.length,
+            magnification: 1,
             itemBuilder: (context, index) {
               var model = DurationUiModel.all[index];
-              return Text(model.title);
+              return Container(
+                alignment: Alignment.center,
+                child: Text(
+                  model.title,
+                  style: textTheme.display1,
+                ),
+              );
             },
             onSelectedItemChanged: (int value) {
               //TODO change delegate
--- lib/src/screens/setup/setup_screen.dart
+++ lib/src/screens/setup/setup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:russian_hat/src/screens/setup/pages/team_setup_page.dart';
+import 'package:russian_hat/src/screens/setup/pages/time_setup_page.dart';
 
 class SetupScreen extends StatelessWidget {
   @override
@@ -15,9 +16,13 @@ class SetupScreen extends StatelessWidget {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 40),
             child: PageView.builder(
-              itemCount: 1,
+              itemCount: 2,
               itemBuilder: (context, index) {
-                return TeamSetupPage();
+                switch (index) {
+                  case 0: return TeamSetupPage();
+                  case 1: return TimeSetupPage();
+                  default: return Text('error');
+                }
               },
             ),
           ),
